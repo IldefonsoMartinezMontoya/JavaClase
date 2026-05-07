a40a31cf92462451dd23295246617a56dc34a282
Ildefonso Martínez Montoya <imarmon1808@g.educaand.es>
2026-05-07 10:14:36 +0200

    Terminar Ejercicio 5

--- Tema 8/src/DiseñoDeLayouts/Ejercicio4.java
+++ Tema 8/src/DiseñoDeLayouts/Ejercicio4.java
@@ -6,14 +6,33 @@ import java.awt.*;
 public class Ejercicio4 extends JFrame {
     public Ejercicio4() {
         setTitle("Formulario de Entrada");
-        setSize(400, 200);
+        setSize(400, 150);
         setResizable(false);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        setLayout(new GridLayout());
+        setLayout(new GridLayout(3, 2, 5, 5));
 
-        JLabel label = new JLabel("Raul pitogordo");
-        add(label);
+        JPanel panel1 = new JPanel();
+        JLabel labelUsuario = new JLabel("Nombre de usuario:", SwingConstants.RIGHT);
+        JTextField textoUsuario = new JTextField(20);
+        panel1.add(labelUsuario);
+        panel1.add(textoUsuario);
+
+        JPanel panel2 = new JPanel();
+        JLabel labelPass = new JLabel("Contraseña:", SwingConstants.RIGHT);
+        JPasswordField textoPass = new JPasswordField(20);
+        panel2.add(labelPass);
+        panel2.add(textoPass);
+
+        JPanel panel3 = new JPanel();
+        JButton botonEntrar = new JButton("Entrar en la aplicación");
+        panel3.add(botonEntrar);
+        add(panel1);
+        add(panel2);
+        add(panel3);
         setVisible(true);
+        panel1.setVisible(true);
+        panel2.setVisible(true);
+        panel3.setVisible(true);
     }
 
     static void main() { SwingUtilities.invokeLater(Ejercicio4::new);}
---
+++ Tema 8/src/DiseñoDeLayouts/Ejercicio5.java
@@ -0,0 +1,61 @@
+package DiseñoDeLayouts;
+
+import javax.swing.*;
+import javax.swing.border.MatteBorder;
+import java.awt.*;
+import java.sql.Timestamp;
+import java.text.SimpleDateFormat;
+import java.time.Instant;
+import java.util.Date;
+
+public class Ejercicio5 extends JFrame{
+    String fechaActual = new SimpleDateFormat("MM/yyyy").format(new Date());
+    public Ejercicio5() {
+        setTitle("Reproductor Multimedia");
+        setSize(700, 300);
+        setResizable(false);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setLayout(new BorderLayout(10, 0));
+
+        JPanel panel1 = new JPanel();
+        panel1.setLayout(new GridLayout(1, 3));
+        panel1.setBorder(new MatteBorder(0, 0, 2, 0, Color.BLACK));
+
+        JLabel fecha = new JLabel(fechaActual, SwingConstants.CENTER);
+        panel1.add(fecha);
+        JLabel tiempo = new JLabel("00:00 / 04:03", SwingConstants.CENTER);
+        panel1.add(tiempo);
+        JLabel formato = new JLabel("Formato: MP3", SwingConstants.CENTER);
+        panel1.add(formato);
+        add(panel1, BorderLayout.NORTH);
+        panel1.setVisible(true);
+
+        JPanel panel2 = new JPanel();
+        panel2.setLayout(new BorderLayout(2, 2));
+        panel2.setBorder(new MatteBorder(0, 0, 2, 0, Color.BLACK));
+
+        JLabel musica = new JLabel("Supersubmarina - Extrema Debilididad", SwingConstants.CENTER);
+        panel2.add(musica);
+        add(panel2, BorderLayout.CENTER);
+        setVisible(true);
+        panel2.setVisible(true);
+
+        JPanel panel3 = new JPanel();
+        panel3.setLayout(new GridLayout(1, 4));
+
+        JButton atras = new JButton("⏮");
+        panel3.add(atras);
+        JButton pauseplay = new JButton(" ⏯");
+        panel3.add(pauseplay);
+        JButton adelante = new JButton("⏭");
+        panel3.add(adelante);
+        JButton parar = new JButton("⏹");
+        panel3.add(parar);
+        add(panel3, BorderLayout.SOUTH);
+        panel3.setVisible(true);
+
+        setVisible(true);
+    }
+
+    static void main() { SwingUtilities.invokeLater(Ejercicio5::new); }
+}
